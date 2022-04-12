3ccdeeb21f9ea0fed52bae133ad09d354af959e4
naufalrezkyananda08@gmail.com <naufalrezkyananda08@gmail.com>
2022-04-12 21:48:12 +0700

    migrate

---
+++ database/seeders/wisata_seeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+class wisata_seeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'naufal',
+            'email' => 'naufal@gmail.com',
+            'password' => 'naufal'
+        ]);
+    }
+}
